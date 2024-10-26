5ef28bb05febd4f5cfea320fbe98b67123a45109
Ayush Raj <86308958+AYUSH18832@users.noreply.github.com>
2024-10-26 23:18:28 +0530

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import re 
+import math
+import hashlib
+import requests
+
+pass_root=Tk()
+
+#Geometry 
+pass_root.geometry("500x400")
+pass_root.minsize(500,400)
+pass_root.maxsize(500,400)
+
+passL=Label(text="ENTER PASSWORD")
+passL.pack()
+
+#GUI
+
+pass_root.mainloop()
+
